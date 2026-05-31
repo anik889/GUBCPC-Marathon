f08ed806aba3e4779eff136b057fe3201a38d6ed
anik889 <rifatchowdhury679@gmail.com>
2026-05-31 22:15:03 +0600

    update

---
+++ Beginner Batch 3.0 Marathon 1/CodeForces-2185A_PerfectRoot.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    for(int tc=0;tc<t;tc++){
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+		cout<<i<<" ";
+		cout<<"\n";
+    }
+}
